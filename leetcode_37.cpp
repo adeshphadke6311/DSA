3adb47535ff614724f15aa8d2b98ad1315c04d7a
Adesh Phadke <adeshphadke6311@gmail.com>
2026-02-23 16:56:45 +0530

    // Time Complexity: O(9^E), where E is number of empty cells // Space Complexity: O(E) due to recursion stack (O(1) for fixed 9x9 board)

---
+++ leetcode_37.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    //TC = O(9^empty cells)
+    //SC = O(1)
+public:
+    bool isSafe(vector<vector<char>> & board, int row, int col, char dig){
+        //Horizontal
+
+        for(int i=0; i<9; i++){
+            if(board[row][i] == dig){
+                return false;
+            }
+        }
+
+        //Vertical 
+
+        for(int i=0; i<9; i++){
+            if(board[i][col] == dig){
+                return false;
+            }
+        }
+
+        //Checking in grid
+
+        int srow = (row/3)*3;
+        int scol = (col/3)*3;
+
+        for(int i=srow; i <= srow+2; i++){
+            for(int j=scol;  j <= scol+2; j++){
+                if(board[i][j] == dig){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool sudokuSolver(vector<vector<char>> & board, int row, int col){
+        if(row == 9){
+            return true;
+        }
+
+        int nextRow = row;
+        int nextCol = col+1;
+
+        if(nextCol == 9){
+            nextCol = 0;
+            nextRow = row+1;
+        }
+        if(board[row][col] != '.'){
+            return sudokuSolver(board, nextRow, nextCol);
+
+        }
+
+        for(char dig = '1'; dig <= '9'; dig++){
+            if(isSafe(board, row, col, dig)){
+                board[row][col] = dig;
+                if(sudokuSolver(board, nextRow, nextCol )){
+                    return true;
+                }
+                board[row][col] = '.';
+            }
+
+        }
+        return false;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        int row = 0, col = 0;
+        sudokuSolver(board,row, col);
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<vector<char>> board = {
+  {'5','3','.','.','7','.','.','.','.'},
+  {'6','.','.','1','9','5','.','.','.'},
+  {'.','9','8','.','.','.','.','6','.'},
+  {'8','.','.','.','6','.','.','.','3'},
+  {'4','.','.','8','.','3','.','.','1'},
+  {'7','.','.','.','2','.','.','.','6'},
+  {'.','6','.','.','.','.','2','8','.'},
+  {'.','.','.','4','1','9','.','.','5'},
+  {'.','.','.','.','8','.','.','7','9'}
+};
+    sol.solveSudoku(board);
+
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
