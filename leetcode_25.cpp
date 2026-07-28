1ba83ba476b1e1838afca8c3553b3bebcc5e49d7
Adesh Phadke <adeshphadke6311@gmail.com>
2026-07-28 16:15:27 +0530

    Added a iterative solution

--- leetcode_25.cpp
+++ leetcode_25.cpp
@@ -9,12 +9,15 @@ using namespace std;
     ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 
+
 class Solution {
     //Time Complexity   :   O(n)
     //Space Complexity  :   O(n/k)
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        
+        //Recursive Approach
+        /*
+
         //Check if k node exists
         int count = 0;
         ListNode* temp = head;
@@ -42,9 +45,54 @@ public:
             count++;
         }
         return newHead;
+
+        */
+
+
+        //Iterative Approach
+        //Time Complexity   :   O(n)
+        //Space Complexity  :   O(1)
+        
+        if(!head || k == 1){
+            return head;
+        }
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prevGroup = &dummy;
+
+        while(true){
+            //Find the kth node from prevGroup
+            ListNode* kth = prevGroup;
+
+            for(int i=0; i<k && kth; i++){
+                kth = kth->next;
+            }
+
+            if(!kth){
+                break;
+            }
+
+            //Reverse current Group
+
+            ListNode* groupNext = kth->next;
+            ListNode*prev = groupNext;
+            ListNode* curr = prevGroup->next;
+
+            while(curr != groupNext){
+                ListNode* next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            //Connect reverse group
+
+            ListNode* temp = prevGroup->next;
+            prevGroup->next = kth;
+            prevGroup = temp;
+
+        }
+        return dummy.next;
     }
 };
-
-int main(){
-    return 0;
-}
