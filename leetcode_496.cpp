cfd255159bebe6f8e627a2f4be5adf6bdf5c2e19
Adesh Phadke <adeshphadke6311@gmail.com>
2026-08-04 14:18:48 +0530

    Added a solution for leetcode 496. Next Greater Element

---
+++ leetcode_496.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<unordered_map>
+using namespace std;
+class Solution {
+    //Time Complexity   :   O(n)
+    //Space Complexity  :   O(n)
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        stack<int> st;
+        unordered_map<int,int> mp;
+
+        for(int i=nums2.size()-1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                mp[nums2[i]] = -1;
+            }else{
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int i=0; i<nums1.size(); i++){
+            ans.push_back(mp[nums1[i]]);
+        }
+        return ans;
+        
+    }
+};
+
+int main(){
+    return 0;
+}
