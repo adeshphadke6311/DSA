0547742847cfe2765cd430098c07e24ae34bc4e0
Adesh Phadke <adeshphadke6311@gmail.com>
2026-08-24 23:50:14 +0530

    Solution for Leetcode 303 - Range Sum Query - Immutable

---
+++ leetcode_303.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class NumArray {
+    vector<int> prefixSum;
+public:
+    NumArray(vector<int>& nums) { //O(n)
+        int n = nums.size();
+
+        prefixSum.resize(n+1);
+        prefixSum[0] = 0;
+
+        for(int i=0; i<n; i++){
+            prefixSum[i+1] = prefixSum[i] + nums[i];
+        }
+        
+    }
+    
+    int sumRange(int left, int right) {//O(1)
+        return prefixSum[right+1] - prefixSum[left];
+        
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
