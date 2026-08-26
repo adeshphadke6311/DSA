b9e6bcac9cfc4bc3bbe14b5490c72cecd2c273dd
Adesh Phadke <adeshphadke6311@gmail.com>
2026-08-26 15:25:20 +0530

    Optimized search operation by using unordered_map

--- leetcode_105.cpp
+++ leetcode_105.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 //Definition for a binary tree node.
@@ -12,12 +13,14 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+
 class Solution {
-        // Time Complexity   : O(n^2) in worst case
-        // Space Complexity  : O(h) auxiliary space
-        //                     O(log n) for balanced tree
-        //                     O(n) for skewed tree
+        // Time Complexity   : O(n) average, O(n^2) Worst case [Due to hash collision]
+        // Space Complexity  : O(n)
+        //                      O(n) due to unordered_map
+        //                      O(h) recursion stack
 public:
+    /*
     int search(vector<int>& inorder, int left, int right, int val){ //O(n)
         for(int i=left; i<=right; i++){
             if(inorder[i] == val){
@@ -27,20 +30,30 @@ public:
         return -1; //This line will never execute
     }
     //O(n)
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int &preIdx, int left, int right){
+
+    */
+
+  
+
+    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int &preIdx, int left, int right, unordered_map<int,int>&mp){
         if(left > right) return nullptr;
 
         TreeNode* root = new TreeNode(preorder[preIdx]);
-        int inIdx = search(inorder, left,right, preorder[preIdx]);
+        
+        int inIdx = mp[preorder[preIdx]];
         preIdx++;
-        root->left = helper(preorder, inorder, preIdx, left, inIdx-1);
-        root->right = helper(preorder, inorder, preIdx, inIdx+1, right);
+        root->left = helper(preorder, inorder, preIdx, left, inIdx-1, mp);
+        root->right = helper(preorder, inorder, preIdx, inIdx+1, right, mp);
 
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int preIdx = 0;
-        return helper(preorder, inorder, preIdx, 0, inorder.size()-1);
+        unordered_map<int, int> mp;
+        for(int i=0; i<inorder.size(); i++){
+            mp[inorder[i]] = i;
+        }
+        return helper(preorder, inorder, preIdx, 0, inorder.size()-1,mp);
         
     }
 };
