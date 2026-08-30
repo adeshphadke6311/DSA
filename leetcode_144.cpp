b161403cf162beabaf17d2d6d2f99225bb9f3348
Adesh Phadke <adeshphadke6311@gmail.com>
2026-08-30 21:26:42 +0530

    Solution for Leetcode 144. Preorder Traversal of a Binary Tree

---
+++ leetcode_144.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    //  Time Complexity     :   O(n)
+    //  Space Complexity    :   O(h) h - Height of Tree
+    //                          O(n) In case of Skewed Tree
+public:
+    
+    
+    void helper(TreeNode* root, vector<int> &ans){
+        if(root == nullptr) return;
+
+        ans.push_back(root->val);
+        helper(root->left, ans);
+        helper(root->right, ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        helper(root, ans);
+        return ans;
+   
+    }
+};
