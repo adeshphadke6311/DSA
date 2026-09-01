ba4945c2d2917d376cecd6ad57f5359713decef6
Adesh Phadke <adeshphadke6311@gmail.com>
2026-09-01 19:29:18 +0530

    Solution for Leetcode 145 - Postorder Traversal of Binary Tree

---
+++ leetcode_145.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    //Time Complexity   :   O(n)
+    //Space Complexity  :   O(h) h - Height of Tree  (Only auxiliary space is considered)
+    //                      O(n) In case of Skewed Tree
+public:
+    void helper(TreeNode* root, vector<int>&ans){
+        if(root == nullptr) return;
+
+        helper(root->left, ans);
+        helper(root->right, ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        helper(root, ans);
+        return ans;
+        
+    }
+};
